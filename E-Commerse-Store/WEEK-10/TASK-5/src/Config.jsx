a20e8022b55b58ed17b8c82f86a249289b6130e4
Meet Patel <meet.p@huptechweb.com>
2025-02-18 12:03:07 +0530

    handled Everything

--- E-Commerse-Store/WEEK-10/TASK-5/src/Config.jsx
+++ E-Commerse-Store/WEEK-10/TASK-5/src/Config.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://e-commerce-website-backend-3byxygvp9.vercel.app"
+export const BASE_URL = "https://e-commerce-website-backend-five.vercel.app"
+// https://e-commerce-website-backend-3byxygvp9.vercel.app
