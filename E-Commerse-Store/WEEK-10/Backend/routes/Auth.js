95c401d6e9b944796e8c056641ce3a6f71fd68c9
Meet Patel <meet.p@huptechweb.com>
2025-02-19 17:13:14 +0530

    Email design changed

--- E-Commerse-Store/WEEK-10/Backend/routes/Auth.js
+++ E-Commerse-Store/WEEK-10/Backend/routes/Auth.js
@@ -11,6 +11,7 @@ const accessTokenTime = 60 * 60 * 1000
 const refreshTokenTime = 24 * 60 * 60 * 1000 * 3
 
 const sendMail = (data) => {
+    let text = ''
     const mailTransport = nodemailer.createTransport({
         service: 'Gmail',
         host: 'smtp.gmail.com',
@@ -19,11 +20,32 @@ const sendMail = (data) => {
             pass: process.env.MAIL_PASSWORD
         }
     })
+    if (data.about === 'Signup') {
+        text = ` <div style="font-family: Arial, sans-serif; color: #333;">
+        <h1 style="color: #007bff;text-align:"center">SOUTH TEXAS SLIGNS</h1>
+          <h2 style="color: #007bff;">Welcome, ${data.username}!</h2>
+          <img src="https://img.freepik.com/free-vector/man-with-hand-up-icon_24877-81630.jpg?ga=GA1.1.841956205.1736584141&semt=ais_hybrid" width="100" height="200"/>
+          <p>Thank you for signing up for our service. We’re excited to have you on board.</p>
+          <p>Click the button below to verify your email and get started:</p>
+          <p>If you didn’t sign up, please ignore this email.</p>
+          <p>Best regards,<br>South Texas Slings</p>
+        </div>`
+    }
+    else if (data.about == 'Login') {
+        text = ` <div style="font-family: Arial, sans-serif; color: #333;">
+        <h1 style="color: #007bff;text-align:"center">SOUTH TEXAS SLIGNS</h1>
+          <h2 style="color: #007bff;">Welcome Back !!</h2>
+          <img src="https://img.freepik.com/free-vector/man-with-hand-up-icon_24877-81630.jpg?ga=GA1.1.841956205.1736584141&semt=ais_hybrid" width="100" height="200"/>
+          <p>Explore amazing Products.</p>
+          <p>Best regards,<br>South Texas Slings</p>
+        </div>`
+    }
+
     const mailOptions = {
         from: process.env.MAIL_USER,
         to: data.email,
         subject: data.subject,
-        text: data.text
+        text: text,
     }
     mailTransport.sendMail(mailOptions, (err, info) => {
         if (err) {
@@ -60,9 +82,7 @@ route.post('/signup', async (req, res) => {
         if (!accessToken || !refreshToken) {
             return res.status(400).json({ message: 'Something went wrong try again !!', success: false })
         }
-        let text = `Hello ${firstname} ${lastname},
-        Thank you for signing up ! We’re thrilled to have you on board.`
-        sendMail({ email, subject: `Welcome to South Texas Slings - Let's Get Started!`, text })
+        sendMail({ email, username: `${firstname} ${lastname}`, subject: `South Texas Slings !`, about: 'Signup' })
         res.status(200).json({ message: 'Successfully Registered', success: true, accessToken, refreshToken, id: user._id })
     }
 })
@@ -87,6 +107,7 @@ route.post('/login', async (req, res) => {
             if (user.role === 'admin') {
                 return res.status(200).json({ message: 'Successfully Logged in', success: true, accessToken, refreshToekn, id: user._id, role: user.role })
             }
+            sendMail({ email, subject: `South Texas Slings !`, about: 'Login' })
             res.status(200).json({ message: 'Successfully Logged in', success: true, accessToken, refreshToekn, id: user._id, role: user.role })
         }
         else {
